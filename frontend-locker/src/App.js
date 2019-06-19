5565f2a949366243df921f54c283c01f33ce487d
nirattikan <5831302008@lamduan.mfu.ac.th>
2019-06-19 23:13:11 +0700

    init

---
+++ frontend-locker/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Form from './components/forms/FormRegister';
+import BUTTON from './layouts/Button';
+import FORMFG from './components/forms/FormForgotPass';
+import Checkout from './components/Checkout';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+      {/* <BUTTON /> */}
+      <Checkout />
+
+
+    </div>
+  );
+}
+
+export default App;
